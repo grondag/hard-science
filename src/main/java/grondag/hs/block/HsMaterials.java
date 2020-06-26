901fcaa1846f442e0b3b51ee045cd41e9be0710a
Grondag <grondag@gmail.com>
2020-06-25 19:57:59 -0700

    Update HsMaterials.java

--- src/main/java/grondag/hs/block/HsMaterials.java
+++ src/main/java/grondag/hs/block/HsMaterials.java
@@ -9,12 +9,12 @@ public abstract class HsMaterials {
 	private HsMaterials() { }
 
 	public static final Material DURACRETE = new FabricMaterialBuilder(MaterialColor.STONE).build();
-	public static final Material DURAGLASS = new FabricMaterialBuilder(MaterialColor.AIR).build();
+	public static final Material DURAGLASS = new FabricMaterialBuilder(MaterialColor.CLEAR).build();
 	public static final Material DURAWOOD = new FabricMaterialBuilder(MaterialColor.WOOD).build();
 	public static final Material DURASTEEL = new FabricMaterialBuilder(MaterialColor.IRON).build();
 
 	public static final Material HYPERCRETE = new FabricMaterialBuilder(MaterialColor.STONE).blocksPistons().build();
-	public static final Material HYPERGLASS = new FabricMaterialBuilder(MaterialColor.AIR).blocksPistons().build();
+	public static final Material HYPERGLASS = new FabricMaterialBuilder(MaterialColor.CLEAR).blocksPistons().build();
 	public static final Material HYPERWOOD = new FabricMaterialBuilder(MaterialColor.WOOD).blocksPistons().build();
 	public static final Material HYPERSTEEL = new FabricMaterialBuilder(MaterialColor.IRON).blocksPistons().build();
 }
